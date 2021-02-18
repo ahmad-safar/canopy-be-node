7a5106d3cc7ac50c4866a8fde34a5c14e578a6a2
Kasjful Kurniawan <kasful.bpp@gmail.com>
2021-02-18 15:33:32 +0800

    Install JWT

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-// Create express app
-var express = require("express")
-var app = express()
+let express = require("express")
+let app = express()
 
-// Server port
-var HTTP_PORT = 8000 
-// Start server
+let HTTP_PORT = 8000 
+
 app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
 });
-// Root endpoint
+
 app.get("/", (req, res, next) => {
     res.json({"message":"Ok"})
 });
 
-// Insert here other API endpoints
-
-// Default response for any other request
 app.use(function(req, res){
     res.status(404);
 });
